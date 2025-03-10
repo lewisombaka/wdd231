fb46d301793bf7d74ac70982b4eea0344f7b3927
lewisombaka <lewisochieng99@gmail.com>
2025-03-10 19:27:59 +0300

    if else

--- JS/main.js
+++ JS/main.js
@@ -38,3 +38,60 @@ random = Math.floor(Math.random() * 5 + 1);
 console.log(reverbedName[random]);
 console.log(typeof(random)); 
 console.log(myName.charAt(Math.floor(Math.random()*5+1)));
+
+const anyName = "Jonathan";
+stringLength = anyName.length;
+console.log(stringLength);
+
+// if statements
+// if (condition){
+    //run some code
+// }
+// else {
+    // run some code
+// }
+
+let soup = "chicken noodle soup";
+let reply;
+let customerIsBanned = true;
+
+if (customerIsBanned){
+    reply = "No Soup For You";
+}
+else if (soup) {
+    reply = `Here's your order of ${soup}`;
+}
+else {
+    reply = `Sorry, we are out of ${soup}`;
+}
+
+console.log(reply);
+
+// switch case rock paper scissor
+
+// ternary operator
+
+let meat = "Beef";
+
+let response = soup ? "Yes it's beef":"No it's not";
+console.log(response);
+
+// user input
+// let myBoolean = confirm("OK === True\nCancel === False"); // confirm vs alert
+// console.log(myBoolean);
+// let myMame = prompt("Please enter your name");
+// if (myMame != null){
+//     console.log(myMame);
+// }
+// else {
+//     console.log("You didn't enter your name");
+// }
+
+let playGame = confirm("Shall we play Rock, Paper, Scissor Game? ");
+
+if (playGame){
+    //
+}
+else{
+    alert("Okay, Please Come Back Soon!");
+}
