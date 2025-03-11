beb76d5b54661134cf4ce38a97bc58e32b414d80
lewisombaka <lewisochieng99@gmail.com>
2025-03-11 20:09:14 +0300

    js

---
+++ recap/dom/dom.js
@@ -0,0 +1,26 @@
+// DOM Manipulation
+
+// GetElementByID()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// GetElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+// getElementByTagName();
+
+const div = document.getElementsByTagName('div');
+console.log(div);
+
+// querySelector()
+
+const container = document.querySelector('div');
+console.log(container);
+
+// querySelectorAll()
+
+const container2 = document.querySelectorAll('div');
+console.log(container2);
